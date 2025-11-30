f7d64a3aa10d81e420f70750cce44e639db1cb9b
Gabriel Carneiro <gecgpro12@gmail.com>
2025-11-30 13:45:16 -0400

    Añadida opción de seleccionar carpeta le falta pulir pero ya funciona, espero...

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import utils  # Importamos tus herramientas
 import datetime
 import auditor
+import os
 
 # Estos imports darán error hasta que tus compañeros creen sus funciones.
 # Por ahora los dejaremos comentados o simularemos que funcionan.
@@ -9,15 +10,18 @@ import auditor
 # import analyzer
 
 # import reports
+carpeta = "./test_samples"
 
 def mostrar_menu_principal():
     """Despliega las opciones del sistema."""
     utils.mostrar_encabezado("KIT DE AUTOMATIZACIÓN DE ARCHIVOS (v1.0)")
+    print(f"Carpeta=  {carpeta}")
     print("1. [Organizador]  Clasificar y ordenar archivos")
     print("2. [Analizador]   Buscar patrones y contenido")
     print("3. [Auditor]      Detectar cambios en carpetas")
     print("4. [Reportes]     Generar informes (CSV/TXT)")
-    print("5. Salir")
+    print("5. Cambiar carpeta")
+    print("6. Salir")
     print("-" * 60)
 
 def mostrar_menu_auditor():
@@ -28,7 +32,16 @@ def mostrar_menu_auditor():
     print("3. Salir")
     print("-" * 60)
 
+def mostrar_menu_seleccion_carpeta():
+    """Despliga el menu de carpetas"""
+    utils.mostrar_encabezado("SELECCIONADOR DE CARPETAS (v1.0)")
+    print(f"Carpeta actual: {carpeta}")
+    print("1. Cambiar carpeta")
+    print("2. Salir")
+    print("-" * 60)
+
 def main():
+    global carpeta
     tiempo_inicio = datetime.datetime.now().strftime("%Y-%m-%d %H-%M")
     while True:
         mostrar_menu_principal()
@@ -49,10 +62,10 @@ def main():
                 mostrar_menu_auditor()
                 opcion = input(">> Selecciona una opción (1-5): ").strip()
                 if opcion == "1":
-                    auditor.generar_snapshot("./test_samples",tiempo_inicio)
+                    auditor.generar_snapshot(carpeta,tiempo_inicio)
                     input("Presiona Enter para volver...")
                 elif opcion == "2":
-                    auditor.generar_reporte("./test_samples",tiempo_inicio)
+                    auditor.generar_reporte(carpeta,tiempo_inicio)
                     input("Presiona Enter para volver...")
                 elif opcion == "3":
                     break
@@ -67,6 +80,31 @@ def main():
             input("Presiona Enter para volver...")
 
         elif opcion == "5":
+            #Menu para cambiar la carpeta
+            while True:
+                mostrar_menu_seleccion_carpeta()
+                opcion = input(">> Selecciona una opción (1-2): ").strip()
+                if opcion == "1":
+                    while True:
+                        utils.limpiar_pantalla()
+                        ruta = input("Introduzca la ruta de la carpeta:")
+                        if os.path.exists(ruta) and ruta != "./":
+                            carpeta = ruta 
+                            break
+                        else:
+                            input("Opción no Valida , presione enter para continuar...")
+                            continue
+                elif opcion == "2":
+                    input("Saliendo, press enter para continuar...")
+                    break
+                else:
+                    print("\n❌ Error: Opción no válida.")
+                    input("Presiona Enter para intentar de nuevo...")
+                    
+                    
+
+
+        elif opcion == "6":
             print("\n¡Hasta luego! Cerrando sistema...")
             break
         
